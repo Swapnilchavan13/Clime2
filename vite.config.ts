01492321e100e5e11630be801d8e6ed7829a8410
Swapnil Chavan <chavanswapnil822@gmail.com>
2026-03-08 15:52:44 +0530

    updated code

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    build: {
+       outDir: "build",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,6 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-     build: {
-       outDir: "build",
-  },
   test: {
     environment: "jsdom",
     globals: true,
